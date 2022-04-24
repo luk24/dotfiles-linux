f1212e0035af1f7047c5e45cd9c12d50d8cf3ae7
Ramsey <luk24.chiu@gmail.com>
2022-04-24 13:45:12 +0800

    Update darwin_dependencies.sh

--- darwin_dependencies.sh
+++ darwin_dependencies.sh
@@ -2,27 +2,12 @@
 
 echo "Checking Homebrew"
 if ! command -v brew >/dev/null 2>&1; then
-   #https://brew.sh
    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 echo "Installing tools..."
 brew install \
-    bash \
-    zsh \
-    ccache \
-    dateutils \
-    gnupg2 \
-    coreutils \
+    zsh-completions \
 
 brew cask install \
     squirrel \
-
-#echo "Installing flock"
-#brew tap discoteq/discoteq && brew install flock
-
-#echo "Something"
-#ln -s /usr/local/bin/lzip /usr/local/bin/lunzip
-
-echo "Copy preset apps"
-cp -rf app/NativeDisplayBrightness.app/ ~/Applications/
